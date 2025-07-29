87cb2e0a48e900facc40e8a4990256c591426fab
kenuuu-x <khojugken@gmail.com>
2025-07-29 22:12:22 +0300

    refactor(db): rename groups table to student_groups and update version

--- app/src/main/java/com/nelsonxilv/gstoutimetable/data/model/GroupDbModel.kt
+++ app/src/main/java/com/nelsonxilv/gstoutimetable/data/model/GroupDbModel.kt
@@ -3,7 +3,7 @@ package com.nelsonxilv.gstoutimetable.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "groups")
+@Entity(tableName = "student_groups")
 data class GroupDbModel(
     @PrimaryKey val groupName: String,
 )
